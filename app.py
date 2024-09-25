c1ea86b1e6c02388888b7b4d9381c9ea9e1a7796
Reese D. <reese@renys.dev>
2024-09-25 16:25:32 -0400

    very smol difference in app.py

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def predict():
 if __name__ == "__main__": 
     print("Loading Keras model...")
     load_model("models/hiragana_latest.keras")
-    app.run(debug=True) # Add debug = True for logs and stuff
+    app.run(debug=True, host='0.0.0.0') # Add debug = True for logs and stuff
